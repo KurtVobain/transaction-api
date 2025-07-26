f12eeef38e3f4b3a4c60a5659b3711a0a80362c3
Evgenii <one1two215@yandex.ru>
2025-07-26 15:47:47 +0200

    Created migration for models

--- transactions/asgi.py
+++ transactions/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for b2broker_project project.
+ASGI config for transactions project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'b2broker_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transactions.settings')
 
 application = get_asgi_application()
--- transactions/wsgi.py
+++ transactions/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for b2broker_project project.
+WSGI config for transactions project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'b2broker_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transactions.settings')
 
 application = get_wsgi_application()
