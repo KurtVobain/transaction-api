e4523e649b8b841e339683eecaa5d0ec1eccec3d
Evgenii <one1two215@yandex.ru>
2025-07-26 16:52:37 +0200

    Added create-transaction view and serializer

---
+++ api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Transaction, Wallet
+
+class TransactionSerializer(serializers.ModelSerializer):
+    wallet_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = ['wallet_id', 'amount', 'txid']
+
+    def validate_wallet_id(self, value):
+        if not Wallet.objects.filter(id=value).exists():
+            raise serializers.ValidationError(f"Wallet {value} not found.")
+        return value
+
+    def validate_txid(self, value):
+        if Transaction.objects.filter(txid=value).exists():
+            raise serializers.ValidationError("Duplicate txid.")
+        return value
+
+    def create(self, validated_data):
+        # 'wallet' injected by the view
+        wallet = validated_data.pop('wallet')
+        return Transaction.objects.create(wallet=wallet, **validated_data)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.db import transaction, IntegrityError
+from rest_framework import generics, serializers
+from .models import Transaction, Wallet
+from .serializers import TransactionSerializer
 
-# Create your views here.
+class TransactionCreateView(generics.CreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+    def perform_create(self, serializer):
+        wallet_id = serializer.validated_data['wallet_id']
+        amount = serializer.validated_data['amount']
+
+        with transaction.atomic():
+            wallet = Wallet.objects.select_for_update().get(id=wallet_id)
+
+            if wallet.balance + amount < 0:
+                raise serializers.ValidationError({
+                    'amount': f'Insufficient funds: {wallet.balance} available.'
+                })
+
+            wallet.balance += amount
+            wallet.save()
+
+            try:
+                serializer.save(wallet=wallet)
+            except IntegrityError:
+                raise serializers.ValidationError({'txid': 'Duplicate txid.'})
